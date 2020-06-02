2f6192927b3b2060c6154b69f1f19c374df8b505
wayne9499 <dheerajsharma9499@gmail.com>
2020-06-02 12:22:46 +0530

    frontend: header implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatMenuModule} from '@angular/material/menu'; 
 const material = [
   MatIconModule,
   MatButtonModule,
-  MatInputModule
+  MatInputModule,
+  MatTooltipModule,
+  MatSelectModule,
+  MatMenuModule
 ]
-
 
 @NgModule({
   declarations: [],
