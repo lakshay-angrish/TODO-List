21702a07119d3db1b52f454c3c60665a96cec798
Lakshay Angrish <angrishlakshay@gmail.com>
2020-06-06 17:18:16 +0530

    linting fix

--- src/app/tasks-layout/tasks-layout.component.ts
+++ src/app/tasks-layout/tasks-layout.component.ts
@@ -20,18 +20,16 @@ export class TasksLayoutComponent implements OnInit {
   H2: string;
   H3: string;
   H4: string;
-  isVisible: boolean = false;
-  isVisible2: boolean = false;
-  
+  isVisible = false;
+  isVisible2 = false;
+
 
   @Input() heading: Text;
   color1 = '#8bd136';
   color2 = '#33e0f1';
-  
 
-  constructor(private data: SearchService,private http: HttpClient, private dialogBox: MatDialog, private reload: ReloadService) {
 
-   }
+  constructor(private data: SearchService, private http: HttpClient, private dialogBox: MatDialog, private reload: ReloadService) { }
 
 
   ngOnInit(): void {
@@ -42,19 +40,16 @@ export class TasksLayoutComponent implements OnInit {
   }
 
   getAllTasks() {
-
-    if(this.searchResults.length == 0){
-
-    this.http.get('http://localhost:3000/allTasks', { responseType: 'json' }).subscribe(
-      (response: any[]) => {
+    if (this.searchResults.length === 0) {
+      this.http.get('http://localhost:3000/allTasks', { responseType: 'json' }).subscribe((response: any[]) => {
         this.tasksToday = [];
         this.tasksUpcoming = [];
         this.tasksCompleted = [];
-         this.H1 = 'Due Today';
-         this.H2 = 'Upcoming';
-         this.H3 = 'Completed';
-         this.isVisible = false;
-         this.isVisible2 = true;
+        this.H1 = 'Due Today';
+        this.H2 = 'Upcoming';
+        this.H3 = 'Completed';
+        this.isVisible = false;
+        this.isVisible2 = true;
 
         for (const task of response) {
           const date = new Date(task.due);
@@ -80,18 +75,17 @@ export class TasksLayoutComponent implements OnInit {
         for (const task of this.tasksCompleted) {
           task.due = new Date(task.due).toDateString();
         }
-    });
-  }else{
-    console.log(this.searchResults.length);
-    this.H4 = 'Search Results';
+      });
+    } else {
+      console.log(this.searchResults.length);
+      this.H4 = 'Search Results';
       this.isVisible = true;
       this.isVisible2 = false;
       for (const task of this.searchResults) {
         task.due = new Date(task.due).toDateString();
       }
+    }
   }
-  }
-
 
   openEditTaskDialog(task) {
     const dialogConfig = new MatDialogConfig();
@@ -103,10 +97,11 @@ export class TasksLayoutComponent implements OnInit {
       await this.getAllTasks();
     });
   }
-  doneTask(id: String) {
-    var isTrue = confirm('click OK to confirm');
+
+  doneTask(ID: string) {
+    const isTrue = confirm('click OK to confirm');
     if (isTrue) {
-      this.http.put('http://localhost:3000/updateStatus', {id: id}, {responseType: 'text'}).subscribe(
+      this.http.put('http://localhost:3000/updateStatus', {id: ID}, {responseType: 'text'}).subscribe(
         (res) => {
           console.log(res);
           this.getAllTasks();
@@ -114,9 +109,10 @@ export class TasksLayoutComponent implements OnInit {
       );
     }
   }
-  goBack(){
-    this.H4 = "";
-    this.searchResults=[];
+
+  goBack() {
+    this.H4 = '';
+    this.searchResults = [];
     this.reload.sendAction(true);
   }
 }
